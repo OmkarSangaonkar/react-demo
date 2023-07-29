2e73da029156268bb1e406e190be12d034c6dd5a
Omkar Sangaonkar <omsangaonkar@gmail.com>
2023-07-29 22:11:11 +0530

    some changes in app

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import "./App.css";
 function App() {
   const [userData, setUserData] = useState({}); // Initialize as an object
 
-  useEffect(() => {
-    axios.get("http://localhost:5555/").then((response) => {
-      console.log("Server Response:", response.data);
-      setUserData(response.data); // Set the JSON object in state
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("https://node-demo-weld.vercel.app/").then((response) => {
+  //     console.log("Server Response:", response.data);
+  //     setUserData(response.data); // Set the JSON object in state
+  //   });
+  // }, []);
 
   return (
     <div className="App">
-      <h1>{userData.name}</h1>
+      <h1>{userData.name || "unknown"}</h1>
     </div>
   );
 }
